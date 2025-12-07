800be68062da2bb0cf07b64befde135532483c7f
Sagi Altaraz <sagi altaraz>
2025-12-07 13:54:27 +0200

    sign up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home/home';
-import Features from './components/features/features';
-//import Stats from './components/stats/Stats';
-import ChatAssistant from './components/ChatAssistant/ChatAssistant';
 import Header from './components/Header/Header';
-//import LogIn from './pages/log-in/log-in';
+
+import HomePage from './pages/home/home';
 import SignupPage from './pages/sign-up/sign-up';
+//import LoginPage from './pages/log-in/log-in';
+
+import Features from './components/features/features';
+import ChatAssistant from './components/ChatAssistant/ChatAssistant';
 
 function App() {
    return (
       <div className="App">
          <Header />
+
          <Routes>
+            {/* home page */}
+            <Route
+               path="/"
+               element={
+                  <>
+                     <HomePage />
+                     <Features />
+                     <ChatAssistant />
+                  </>
+               }
+            />
             <Route path="/sign-up" element={<SignupPage />} />
+            {/* <Route path="/log-in" element={<LoginPage />} /> */}
+            <Route path="*" element={<div>404 – העמוד לא נמצא</div>} />
          </Routes>
-         <Home />
-         <Features />
-         <ChatAssistant />
-         {/* <Stats /> */}
       </div>
    );
 }
