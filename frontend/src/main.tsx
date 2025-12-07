4ac617b0af7daeb3959436d7430ded8b6b012d39
Sagi Altaraz <sagi altaraz>
2025-12-07 12:59:52 +0200

    add page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-   createRoot(rootElement).render(
-      <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+   <React.StrictMode>
+      <BrowserRouter>
          <App />
-      </StrictMode>
-   );
-} else {
-   console.error('Root element not found!');
-}
+      </BrowserRouter>
+   </React.StrictMode>
+);
